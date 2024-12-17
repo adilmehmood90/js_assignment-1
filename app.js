e18d29b55911d9e45a5c9817be8d6543a1c87e88
adilmehmood <adilarain2120@gmail.com>
2024-12-18 00:25:35 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,79 @@
+//Chapter No 1 Alerts
+
+//Question No 1
+// alert("Welcome to Java Script.");
+
+//Question No 2
+// alert("Error! Please enter valid password.");
+
+//Question No 3
+// alert("Welcome to JS Land \n Happy Coding");
+
+// Question No 4
+// alert("Welcome to JS Land");
+// alert("Happy Coding");
+
+//Question No 5
+// console.log("Hello..I can run JS through my web browsers console");
+
+//******************************************* */
+
+//Chapter No 2 Variables for Strings
+
+//Question No 1
+// var myName= ("Adil Mehmood");
+// alert(myName);
+
+//Question No 2
+// var message= ("Hello World");
+// alert(message);
+
+//Question No 3
+// var bioData= ("Certified Mobile Application Development");
+// alert(bioData);
+
+//Question No 4
+// var foodName=("Pizza\nPizz\nPiz\nPi\nP");
+// alert(foodName);
+
+//Question No 5
+// var email=("adil.arain2120@gmail.com");
+// alert(email);
+
+//Question No 6
+// var book=("A smarter way to learn Java Script");
+// alert(book);
+
+//Question No 7
+// document.write("<h1>Yall! I can write HTML content throu h JavaScrit.</h1>")
+
+//************************************************ */
+
+
+//Chapter No 3 Variables for Numbers
+
+//Question No 1
+// var age=(32);
+// alert(age);
+
+//Question No 2
+// var visitors=("You have visited this site 14 times.");
+// alert(visitors);
+
+//Question No 3
+// var birthYear="1988"
+// document.write("<h4>" + birthYear + "</h4>");
+
+//Question No 4
+// var customerName = (" Yasir Ali ordered \n");
+// var quantity = (4);
+// var product = ("T-shirt(s) on \n");
+// var store = ("adidas Brand")
+// document.write(customerName + quantity + product + store)
+
+
+
+
+
+
+
